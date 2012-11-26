3b33f77b7ebd547287b294889d7e59b2b7850f62
viniciusmo <viniciusmo@HP-ProBook.(none)>
2012-11-26 13:45:55 -0500

    Adicionando metodo para calcular os pontos das atividades

--- regionais/PENA.cpp
+++ regionais/PENA.cpp
@@ -3,10 +3,12 @@
   by Vinicius Moises de Oliveira - FATEC - Carapicuiba
   vinicius.moises.oliveira@gmail.com
 */
+
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
 #include <string.h>
+#include <string>
 #include <map>
 
 using namespace std;
@@ -17,15 +19,17 @@ class Task{
   public :
    int id;
    int point;
+   char  day_of_week[100];
    int start_time_minute;
    int finish_time_minute;
-   char * day_of_week;
+
+   
 };
 
-/* Simulate instance variables */
-map < char * , vector < Task * > > tasks;
+map < string , vector < Task * > > tasks;
 char DAYS_OF_WEEK [][5] = {"Seg", "Ter", "Qua" , "Qui", "Sex"};
 int TOTAL_DAYS_OF_WEEK = 5;
+int memorization_result[10000];
 
 
 int convert_time_day_to_int (char  time_of_day [] ){
@@ -48,39 +52,95 @@ void init_tasks_with_days_of_week(){
 	}
 }
 
+void init_memorization_result(){
+   for (int i = 0; i < 10000+1;i++){
+       memorization_result[i] = -1;
+   }
+}
+
 int compare (Task * t1 , Task * t2){
    return t1->finish_time_minute < t2->finish_time_minute;	
 }
 
+int find_largest_index_smaller_by(vector<Task*> tasks , Task * task , int index_task){
+
+	for (int i = index_task ; i >=0 ; i--){
+		Task * atual_task =  tasks[i];
+		if (atual_task->finish_time_minute <= task->start_time_minute){
+
+		    return i;
+		}	
+	}
+    return -1;
+}
+
+
+
+int maximum_points_tasks (vector<Task *> tasks , int j){
+    
+    if (j == -1 ){
+		return 0;
+	} else{
+	    int pj = find_largest_index_smaller_by (tasks,tasks[j],j);
+		return max(tasks[j]->point + maximum_points_tasks(tasks,pj),maximum_points_tasks(tasks,j-1));
+	}
+}
+
+int maximum_points_tasks_faster (vector<Task *> tasks , int j){
+   
+    if (j == -1){
+       return 0;
+    }else if (memorization_result[j] != -1){
+   	   return memorization_result[j];
+    }else {
+       int pj = find_largest_index_smaller_by (tasks,tasks[j],j);
+       memorization_result[j] = max(tasks[j]->point + maximum_points_tasks_faster(tasks,pj),maximum_points_tasks_faster(tasks,j-1));
+       return memorization_result[j];
+    }
+
+}
+
+
+
+
 int main (){
 	int n;
-
+    int result[5];
+    int total = 0;
 	while (1){ 
+	
 		 scanf ("%d",&n);
 		 if (n == 0) break;
 		 init_tasks_with_days_of_week();
+		 init_memorization_result();
 		 for (int i = 0; i < n; i++){
-		 	char * start_time_read;
-		 	char * finish_time_read;
-
-		 	Task * task  = new Task();
-		 	scanf ("%d",&task->id);
-		 	scanf ("%d",&task->point);
-		 	scanf ("%s",task->day_of_week);
-		 	scanf ("%s",start_time_read);
-		 	scanf  ("%s",finish_time_read);
-		 	task->start_time_minute  = convert_time_day_to_int (start_time_read);
-		 	task->finish_time_minute  = convert_time_day_to_int (finish_time_read);
-		 	tasks[task->day_of_week].push_back(task);
+		 	 char start_time_read[100];
+		 	 char finish_time_read[100];
+		 	 Task * t  = new Task();
+		     scanf ("%d",&t->id);
+         	 scanf ("%d",&t->point);
+             scanf ("%s",t->day_of_week);
+             scanf ("%s",start_time_read);
+             scanf ("%s",finish_time_read);
+             t->start_time_minute =  convert_time_day_to_int (start_time_read);
+             t->finish_time_minute = convert_time_day_to_int (finish_time_read);
+             string str(t->day_of_week);
+             tasks[str].push_back(t);
 		 }
-
 		 for (int i = 0; i < TOTAL_DAYS_OF_WEEK; i++){
-		 	 vector<Task *> vec = tasks[DAYS_OF_WEEK[i]];
-		 	 sort(vec.begin(),vec.end(),compare);
-
+		 	  string key(DAYS_OF_WEEK[i]);
+		 	  vector<Task*> vec = tasks[key];
+		 	  sort (vec.begin(),vec.end(),compare);
+		 	  result[i] = maximum_points_tasks_faster(vec,vec.size()-1);	
+		 	  total += result[i];	 	
+		 	  init_memorization_result();
 		 }
+		 printf ("Total de pontos: %d\n",total);
+		 for (int i = 0;  i < TOTAL_DAYS_OF_WEEK;i++){
+		    printf ("%s: %d\n",DAYS_OF_WEEK[i],result[i]);
+		 }
+		 total = 0;
 		 tasks.clear();
 	}
-
 	return 0;
 }
